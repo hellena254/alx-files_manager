02039aea814ae50cb8ad66f8edf2ee0693d9a77c
Hellena <hellenah243@gmail.com>
2024-09-03 15:26:40 +0000

    add the api for posting users and getting their details

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ const router = express.Router();
 // define endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
 
 export default router;
