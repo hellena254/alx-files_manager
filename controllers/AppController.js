32aae323395993ef752d515a14b94cf20bc32018
Hellena <hellenah243@gmail.com>
2024-09-03 14:45:51 +0000

    add API

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  // GET /status
+  static async getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+// GET /stats
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    res.status(200).json({
+      users: usersCount,
+      files: filesCount,
+    });
+  }
+}
+
+export default AppController;
